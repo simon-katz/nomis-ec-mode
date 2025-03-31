4a6986bbe91334cf14339663ec1a1c4d76aa406a
Simon Katz <sk@nomistech.com>
2025-03-31 10:54:33 +0100

    Provide nomis-electric-clojure.el for backwards compatibility

---
+++ nomis-electric-clojure.el
@@ -0,0 +1,8 @@
+;;;; nomis-electric-clojure.el --- Minor mode for Electric Clojure ---  -*- lexical-binding: t -*-
+
+;;;; This package has been renamed as `nomis-ec-mode`. This file is provided for
+;;;; backwards compatibility.
+
+(require 'nomis-ec-mode)
+
+(provide 'nomis-electric-clojure)
